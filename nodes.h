13c84f0ee5021909350367cb4308ead3e14d376f
galleg_a <galleg_a@epitech.eu>
2014-03-25 21:21:05 +0100

    aze

--- nodes.h
+++ nodes.h
@@ -5,37 +5,32 @@
 ** Login   <franck_r@epitech.net>
 **
 ** Started on  Tue Mar 25 11:09:02 2014 Romain Franck
-** ##Last update Tue Mar 25 14:45:43 2014 Romain Franck
+** Last update Tue Mar 25 21:12:06 2014 Galleg_a
 */
 
 #ifndef NODES_H_
-#define NODES_H_
+# define NODES_H_
 
-typedef struct s_node
+typedef struct	s_node
 {
+  struct s_node	*prev;
   char		*name;
   char		type;
-  int		dist;
-  struct s_tunl	*path;
-  struct s_node *next;
-}	t_nd;
-
-typedef struct s_tunl
-{
-  t_nd		*exit;
-  char		trafic;
-  struct s_tunl	*next;
-}	t_tn;
+  //int		dist;
+  char		full;
+  struct s_node	*links;
+  struct s_node	*next;
+} t_nd;
 
 /*
 ** Définir l'emplacement de la fourmi selon le node ou la fourmi ?
 */
 
-typedef struct s_ant
+typedef struct	s_ant
 {
   int		number;
   t_nd		*node;
   struct s_ant	*next;
-}	t_ant;
+} t_ant;
 
 #endif
